464eb6d8bf8d6f1211abc41fef11c91ba5f61011
mikeedel <105459492+mikeedel@users.noreply.github.com>
2024-09-24 19:45:43 -0600

    dataframe and series

---
+++ dataframe.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+myDataFrame = pd.DataFrame(
+                {'Yes': [50,41], 
+                'No': [131,2]})
+
+print(myDataFrame)
---
+++ series.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+mySerie = pd.Series([2,3,4,5,6])
+
+print(mySerie)
